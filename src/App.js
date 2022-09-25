fd7d2190951960ecb09b72b08eedfb3d67c310ce
Julia777 <s.julija@me.com>
2022-09-25 20:50:20 +0100

    reinstalled all the packages to react 18, installed npm marked package

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import { useState } from 'react';
+import { marked } from 'marked';
+import './App.css';
 
-export default class App extends React.Component{
-render(){
+function App() {
+  const [text, setText] = useState('');
   return (
     <div className="App">
-     
+      <textarea id="editor" 
+        onChange={(event) => {
+        setText(event.target.value);}}
+        value={text}>
+      </textarea>
+    <div id="preview"
+    dangerouslySetInnerHTML={{
+      __html: marked(text),
+    }}></div>
     </div>
-  );}
+  );
 }
 
-
+export default App;
