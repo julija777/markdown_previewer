c3a9527111ce77343695210f2547752b6b1618a6
Julia777 <s.julija@me.com>
2022-09-25 22:50:53 +0100

    all 8 tests passed

--- src/App.js
+++ src/App.js
@@ -2,8 +2,51 @@ import { useState } from 'react';
 import { marked } from 'marked';
 import './App.css';
 
+/*  When my markdown previewer first loads, 
+the default text in the #editor field should contain valid markdown 
+that represents at least one of each of the following elements:
+
+a header (H1 size), 
+a sub header (H2 size), 
+a link, inline code, 
+a code block, 
+a list item, 
+a blockquote, 
+an image, 
+and bolded text
+ */
+
+
+
 function App() {
-  const [text, setText] = useState('');
+  const [text, setText] = useState(` # H1 
+  ## H2
+  [title](https://www.w3schools.com/) 
+  \`code\` 
+  \`\`\`
+  {
+    "firstName": "John", 
+    "lastName": "Smith",
+    "age": 25
+  }
+  \`\`\`
+    
+  - Write the press release
+  - Update the website
+  - Contact the media
+
+  > blockquote
+
+  ![alt text](image.jpg)
+
+  **bold text**
+
+      `);
+
+  marked.setOptions({
+    breaks: true
+  })
+  
   return (
     <div className="App">
       <textarea id="editor" 
